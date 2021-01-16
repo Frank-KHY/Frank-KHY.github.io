d24a9c8ecf554c78d4db3cf4436299b19d9f4fc7
GWFrank <frank910311@gmail.com>
2021-01-16 21:12:16 +0800

    Switch to using project pages

--- college/common_htmls/head.js
+++
@@ -1,4 +0,0 @@
-document.write( '        <meta charset=\"utf-8\"/>\n' );
-document.write( '        <title>臺大僧指南</title>\n' );
-document.write( '        <link href=\"https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@500;600&display=swap\" rel=\"stylesheet\">\n' );
-document.write( '        <link rel=\"stylesheet\" type=\"text/css\" href=\"styles/style.css\">' );
